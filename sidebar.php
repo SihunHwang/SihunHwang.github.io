c8fd57d38104339d4bfc45270535207813bc2595
SihunHwang <hsihun1130@gmail.com>
2021-01-04 12:10:36 +0900

    rearrangement of sidebar

--- sidebar.php
+++ sidebar.php
@@ -1,6 +1,23 @@
 <nav>
     <a class='logo' href='index.php'>Sihun Hwang</a>
     <div class="dropdown">
+        <button class='dropbtn'>Coding</button>
+        <div class='dropdown-content'>
+            <div class="dropdown">
+                <button class='dropbtn'>Harvard CS50: AI</button>
+                    <div class='dropdown-content'>
+                        <a href='cs50_about.php'>About</a>
+                        <a href='cs50_search.php'>1.Search</a>
+                        <a href='cs50_knowledge.php'>2.Knowledge</a>
+                    </div>
+                </div>
+        <a href='google_foobar.php'>Google foobar</a>
+        <a href='codewars.php'>Codewars</a>
+        <a href='python_webscrapper.php'>Python webscrapper</a>
+          
+        </div>
+    </div>
+    <div class="dropdown">
         <button class='dropbtn'>Projects</button> 
           <div class='dropdown-content'>
               <a href=''>some project</a>
@@ -13,21 +30,6 @@
           <a href=''>some publification</a>
         </div>
     </div>
-    <div class="dropdown">
-        <button class='dropbtn'>Coding</button>
-        <div class='dropdown-content'>
-          <a href='google_foobar.php'>Google foobar</a>
-          <a href='codewars.php'>Codewars</a>
-          <a href='python_webscrapper.php'>Python webscrapper</a>
-          <div class="dropdown">
-          <button class='dropbtn'>Harvard CS50: AI</button>
-              <div class='dropdown-content'>
-                  <a href='cs50_about.php'>About</a>
-                  <a href='cs50_search.php'>1.Search</a>
-                  <a href='cs50_knowledge.php'>2.Knowledge</a>
-              </div>
-            </div>
-        </div>
-    </div>
+    
     <a class='footer' href='https://github.com/SihunHwang'>Github</a>
 </nav>
