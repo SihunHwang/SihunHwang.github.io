d9eb63ccaf3964e90e8450b2c6b53a973c283f0d
SihunHwang <hsihun1130@gmail.com>
2021-01-04 12:15:25 +0900

    link of this website's code added

--- index.php
+++ index.php
@@ -7,6 +7,9 @@
 </head>
 <body>
 <?php include 'sidebar.php'?>
-
+<div class='main'>
+<a style='font-size:20px;padding-left:30px;color:rgb(245, 212, 63)'href='https://github.com/SihunHwang/portfolio_website'>
+    >See the code of this website</a>
+</div>
 </body>
 </html>
