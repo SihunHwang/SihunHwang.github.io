4c41c5bc762d5736df8c13007b4118bbeefbfc7b
SihunHwang <74863441+SihunHwang@users.noreply.github.com>
2021-02-13 00:33:08 +0900

    Update and rename active_menu.js to include.js

--- active_menu.js
+++
@@ -1,7 +0,0 @@
-var current_location = location.href
-        var menu = document.querySelectorAll('.menu')
-        for (var i=0; i<menu.length; i++){
-            if (menu[i].href === current_location){
-                menu[i].className = 'active'
-            }
-        }
